475c4128f26aa7536deb37921cae72454aa1264a
David Yu <lewen.yu@gmail.com>
2012-07-15 22:42:56 -0700

    refactored greedy bot

--- bot/greedy.py
+++ bot/greedy.py
@@ -1,12 +1,9 @@
 import sys
-from map import Controller as C
-from map import MapReader as MR
 from map import MoveType as MT
 from map import w, h, num, tup    #don't initialize your own instance of converter!
 from map import TileType
-from map import MoveType as MT
 
-# Always try to move one of four adjacent squares and never wait. 
+# always try to move one of four adjacent squares and never wait. 
 
 class Greedy():
     def __init__(self):
@@ -23,45 +20,36 @@ class Greedy():
         best_dist = sys.maxint + 1
         best_tile = None
 
-        for (tx, ty) in adj_tiles:
+        for (direction, tx, ty) in adj_tiles:
             dist = 0
             for (lx, ly) in lambdas:
                 dist += self.get_man_dist((tx, ty), (lx, ly)) # use sum dist to all lambdas as heuristic for how bad this position is
             if dist < best_dist:
                 best_dist = dist
-                best_tile = (tx, ty)
+                best_tile = (direction, tx, ty)
         
         assert best_tile != None, 'GreedyBot doesn\'t know where to go!'
 
         (rx, ry) = tup(self.m.robot)
-        (tx, ty) = best_tile
-
-        if rx > tx:
-            return MT.LEFT
+        (direction, tx, ty) = best_tile
         
-        if rx < tx:
-            return MT.RIGHT
+        return direction
 
-        if ry > ty:
-            return MT.DOWN
-
-        if ry < ty:
-            return MT.UP
-
-    def get_adjacent_tiles(self):
+    def get_adjacent_tiles(self):    # returns set of tuples (direction, tile x, tile y)
         (rx, ry) = tup(self.m.robot)
         
-        list_pos = set()
-        if self.walkable(rx-1, ry): #left
-            list_pos.add((rx-1, ry))
-        if self.walkable(rx+1, ry): #right
-            list_pos.add((rx+1, ry)) 
-        if self.walkable(rx, ry-1): #down
-            list_pos.add((rx, ry-1))
-        if self.walkable(rx, ry+1): #up
-            list_pos.add((rx, ry+1)) 
+        adj_set = set()
 
-        return list_pos
+        if self.walkable(rx-1, ry):             #left
+            adj_set.add((MT.LEFT, rx-1, ry))
+        if self.walkable(rx+1, ry):             #right
+            adj_set.add((MT.RIGHT, rx+1, ry)) 
+        if self.walkable(rx, ry-1):             #down
+            adj_set.add((MT.DOWN, rx, ry-1))
+        if self.walkable(rx, ry+1):             #up
+            adj_set.add((MT.UP, rx, ry+1)) 
+
+        return adj_set
 
     def walkable(self, x, y):
         return True if x < w() and x >= 0 and y < h() and y >= 0 and\
